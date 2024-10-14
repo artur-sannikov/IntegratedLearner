cc551695b5691442764221e5336fb8be4c9afaec
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-10-14 14:23:11 +0300

    add: flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Nix Flake for IntegratedLearner R package";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        IntegratedLearner = pkgs.rPackages.buildRPackage {
+          name = "IntegratedLearner";
+          src = self;
+          propagatedBuildInputs = builtins.attrValues {
+            inherit (pkgs.rPackages)
+
+              bartMachine
+              caret
+              cowplot
+              glmnetUtils
+              mcmcplots
+              nloptr
+              quadprog
+              ROCR
+              SuperLearner
+              tidyverse
+              ;
+          };
+        };
+      in
+      {
+        packages.default = IntegratedLearner;
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ IntegratedLearner ];
+          inputsFrom = pkgs.lib.singleton IntegratedLearner;
+        };
+      }
+    );
+}
